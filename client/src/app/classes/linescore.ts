16b20942d34458610c192678218a67e5dc14074d
Jim Murphy <murphyj1011@gmail.com>
2019-07-06 15:29:22 -0400

    added constructors to game, linescore, pitcher classes

--- client/src/app/classes/linescore.ts
+++ client/src/app/classes/linescore.ts
@@ -8,4 +8,15 @@ export class Linescore {
   strikeouts: Count;
   stolenbases: Count;
   errors: Count;
+
+  constructor(inning: [Count], hits: Count, runs: Count, homeruns: Count, strikeouts: Count, 
+    stolenbases: Count, errors: Count) {
+      this.inning = inning;
+      this.hits = hits;
+      this.runs = runs;
+      this.homeruns = homeruns;
+      this.strikeouts = strikeouts;
+      this.stolenbases = stolenbases;
+      this.errors = errors;
+    }
 }
--- client/src/app/classes/pitcher.ts
+++ client/src/app/classes/pitcher.ts
@@ -8,5 +8,15 @@ export class Pitcher {
   number: number;
   win: number;
 
-  constructor() {}
+  constructor(era: number, first: string, id: number, last: string, losses: number, 
+    name_display_roster: string, number: number, win: number) {
+      this.era = era;
+      this.first = first;
+      this.id = id;
+      this.last = last;
+      this.losses = losses;
+      this.name_display_roster = name_display_roster;
+      this.number = number;
+      this.win = win;
+    }
 }
