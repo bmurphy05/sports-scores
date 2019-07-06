1075413c921daf525766a418e45ffc25c9336b57
Jim Murphy <murphyj1011@gmail.com>
2019-07-06 12:58:43 -0400

    edit routing. remove get data button

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { GameDetailComponent } from './game-detail/game-detail.component';
 
 const routes: Routes = [
+  { path: "", redirectTo: "/dashboard", pathMatch: "full" },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'game/:id', component: GameDetailComponent }
 ];
