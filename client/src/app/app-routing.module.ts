1a3fa5046cde6a8964791676cf6539a0d4b1bae0
Jim Murphy <murphyj1011@gmail.com>
2019-07-06 12:49:39 -0400

    add routing.  add game-detail component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GameDetailComponent } from './game-detail/game-detail.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
