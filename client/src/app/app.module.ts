582c4ea962261cd762b28d204fe8c8d5202f2e56
Jim Murphy <murphyj1011@gmail.com>
2019-07-06 23:46:17 -0400

    add search bar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,13 +10,15 @@ import { AppMaterialModule } from './app-material';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { GameDetailComponent } from './game-detail/game-detail.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     DashboardComponent,
-    GameDetailComponent
+    GameDetailComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +26,8 @@ import { GameDetailComponent } from './game-detail/game-detail.component';
     HttpClientModule,
     BrowserAnimationsModule,
     AppMaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/searchbar/searchbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchbar',
+  templateUrl: './searchbar.component.html',
+  styleUrls: ['./searchbar.component.css']
+})
+export class SearchbarComponent implements OnInit {
+  value: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.value = '';
+  }
+
+}
